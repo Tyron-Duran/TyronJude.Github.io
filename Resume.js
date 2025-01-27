547b13f246bc13df0c09323c4a76ce1d236d2e24
Tyron-Duran <167729154+Tyron-Duran@users.noreply.github.com>
2025-01-27 21:11:37 +0800

    Create Resume.js

---
+++ Resume.js
@@ -0,0 +1,6 @@
+document.getElementById("mode-toggle").addEventListener("click", function() {
+    const body = document.body;
+    body.classList.toggle("dark-mode");
+
+    this.textContent = body.classList.contains("dark-mode") ? "Switch to Light Mode" : "Switch to Dark Mode";
+});
